7d02e30305bb35ec1e3e67af70c94abb4998090e
ida <ida@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2012-01-07 11:29:39 +0000

    changed desc of prod

--- setup.py
+++ setup.py
@@ -11,6 +11,9 @@ setup(name='collective.portlet.sitemap',
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
+        "Framework :: Plone :: 3.3",
+        "Framework :: Plone :: 4.0",
+        "Framework :: Plone :: 4.1",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
