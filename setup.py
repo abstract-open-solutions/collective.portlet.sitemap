2c02ca31e69b684309fbce3d74ef29a93f15ab49
ida <ida@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2012-01-07 11:30:46 +0000

    changed desc of prod and added classifiers

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ version = '1.0.3'
 
 setup(name='collective.portlet.sitemap',
       version=version,
-      description="Plone3 navigation portlet extended",
+      description="Plone 3 and 4 navigation portlet extended",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
